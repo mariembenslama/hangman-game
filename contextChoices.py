59b1199e4c3006046141b26323179fe227ebdee5
Mariem ben slama <mariembenslama@localhost.localdomain>
2021-04-06 09:13:03 +0100

    fixed vocabulary and range

--- contextChoices.py
+++ contextChoices.py
@@ -1,30 +1,22 @@
-questionRange = 8
+questionRange = 3
 
 switcher = {
     1: "Animals",
-    2: "Home",
-    3: "Clothes",
-    4: "Food",
-    5: "Body",
-    6: "Jobs",
-    7: "Sports",
-    8: "Colors",
+    2: "Clothes",
+    3: "Food"
 }
 
 vocabulary = {
     1: ["bear", "camel", "cat", "kitten", "cow", "crab", "crocodile", "bird", "deer", "dog", "puppy", "dolphin", "duck", "elephant", "fish", 
         "fox", "giraffe", "goat", "hedgehog", "horse", "jellyfish", "kangaroo", "whale", "lion", "monkey", "octopus", "peacock", "pig",
         "rabbit", "seahorse", "shark", "sheep", "shrimp", "snake", "starfish", "tiger", "turtle", "whale", "wolf", "zebra"]
-    2: [''],
-    3: ["belt", "bikini", "blouse", "boots", "boxers", "bra", "cardigan", "cap", "pants", "coat", "dress",
+    2: ["belt", "bikini", "blouse", "boots", "boxers", "bra", "cardigan", "cap", "pants", "coat", "dress",
         "dress", "gloves", "hat", "hoodie", "jacket", "jeans", "leggings", "mittens", "pyjamas", 
         "panties", "trousers", "pantyhose", "pullover", "raincoat", "scarf", "shirt", "shoes", "shorts",
         "skirt", "socks", "suit", "sweater", "sweatpants", "sweatshirt", "swimsuit", "tie", "tracksuit", "vest"],
-    4: [''],
-    5: [''],
-    6: [''],
-    7: ['twww'],
-    8: ['tweet']
+    3: ["apple", "avocado", "banana", "rice", "cake", "candy", "citron", "curry", "egg", "meat", "beef", 
+        "tea", "garlic", "chicken", "fish", "seafood", "vegetables", "broccoli", "spinach", "carrots", 
+        "onions", "fruit", "sauce", "honey", "hamburger", "cream", "kiwi", "milk", "salad", "yogurt"],
 }
 
 def contextChoice():
@@ -42,7 +34,7 @@ def contextChoice():
 
     elif(int(choice) not in range(1, questionRange + 1)):
 
-        print('Invalid choice, please choose a number between 1 to 8!')
+        print('Invalid choice, please choose a number between 1 to ' + questionRange + '!')
         contextChoice()
 
     else:
