2bac4041f8845f16e512448bda83bb886969b959
Mariem ben slama <mariembenslama@localhost.localdomain>
2021-04-10 12:28:17 +0100

    put vocabulary in a separate resource

---
+++ vocabularies.py
@@ -0,0 +1,21 @@
+questionRange = 3
+
+switcher = {
+    1: "Animals",
+    2: "Clothes",
+    3: "Food"
+}
+
+vocabulary = {
+    1: ["bear", "camel", "cat", "kitten", "cow", "crab", "crocodile", "bird", "deer", "dog", "puppy", 
+        "dolphin", "duck", "elephant", "fish", "fox", "giraffe", "goat", "hedgehog", "horse", "jellyfish", 
+        "kangaroo", "whale", "lion", "monkey", "octopus", "peacock", "pig", "rabbit", "seahorse", "shark", 
+        "sheep", "shrimp", "snake", "starfish", "tiger", "turtle", "whale", "wolf", "zebra"],
+    2: ["belt", "bikini", "blouse", "boots", "boxers", "bra", "cardigan", "cap", "pants", "coat", "dress",
+        "dress", "gloves", "hat", "hoodie", "jacket", "jeans", "leggings", "mittens", "pyjamas", 
+        "panties", "trousers", "pantyhose", "pullover", "raincoat", "scarf", "shirt", "shoes", "shorts",
+        "skirt", "socks", "suit", "sweater", "sweatpants", "sweatshirt", "swimsuit", "tie", "tracksuit", "vest"],
+    3: ["apple", "avocado", "banana", "rice", "cake", "candy", "citron", "curry", "egg", "meat", "beef", 
+        "tea", "garlic", "chicken", "fish", "seafood", "vegetables", "broccoli", "spinach", "carrots", 
+        "onions", "fruit", "sauce", "honey", "hamburger", "cream", "kiwi", "milk", "salad", "yogurt"],
+}
